5e82115d00412d0f3a42062dfaa32be45da44651
rameshy2415 <rameshy2415@gmail.com>
2025-03-26 20:04:50 +0530

    Enhance login btn

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 import { authAPI } from "../services/api";
+
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +13,11 @@ const Login = () => {
   });
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   const navigate = useNavigate();
 
   const { email, password } = formData;
@@ -38,7 +45,7 @@ const Login = () => {
       navigate("/dashboard");
     } catch (err) {
       setLoading(false);
-      setError(err.response?.data.message || "Login failed");
+      setError(err.response?.data?.message || "Login failed");
     }
   };
 
@@ -84,16 +91,29 @@ const Login = () => {
               <label htmlFor="password" className="sr-only">
                 Password
               </label>
+              <div className="relative">
               <input
                 id="password"
                 name="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 required
                 className="w-full p-2 border border-gray-300 rounded focus:ring-2 focus:ring-purple-300 focus:border-transparent"
                 placeholder="Password"
                 value={password}
                 onChange={onChange}
               />
+              <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? (
+                    <EyeSlashIcon className="h-5 w-5" aria-hidden="true" />
+                  ) : (
+                    <EyeIcon className="h-5 w-5" aria-hidden="true" />
+                  )}
+                </button>
+                </div>
             </div>
           </div>
 
@@ -101,9 +121,28 @@ const Login = () => {
             <button
               type="submit"
               disabled={loading}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-300"
+              className="group gap-2 relative w-full flex justify-center cursor-pointer py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:bg-amber-50"
             >
               {loading ? "Signing in..." : "Sign in"}
+              {loading && (
+                <svg
+                  className="animate-spin size-5"
+                  viewBox="0 0 50 50"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    stroke="white"
+                    strokeWidth="6"
+                    strokeLinecap="round"
+                    strokeDasharray="100 200"
+                    strokeDashoffset="0"
+                  />
+                </svg>
+              )}
             </button>
           </div>
 
@@ -116,6 +155,55 @@ const Login = () => {
             </Link>
           </div>
         </form>
+
+        <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
+                    <path d="M12 0C5.37 0 0 5.37 0 12s5.37 12 12 12 12-5.37 12-12S18.63 0 12 0zm.163 18.548c-3.038 0-5.5-2.463-5.5-5.5s2.462-5.5 5.5-5.5c1.481 0 2.811.59 3.807 1.541l-1.6 1.541a3.085 3.085 0 0 0-2.207-.907c-1.897 0-3.444 1.565-3.444 3.425s1.547 3.425 3.444 3.425c1.694 0 2.909-1.212 3.156-2.832h-3.156v-2.017h5.272c.056.282.101.564.101.936 0 3.21-2.151 5.488-5.373 5.488z"/>
+                  </svg>
+                  Google
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                  </svg>
+                  GitHub
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
+                    <path d="M12 0C5.37 0 0 5.37 0 12s5.37 12 12 12 12-5.37 12-12S18.63 0 12 0zm.163 18.548c-3.038 0-5.5-2.463-5.5-5.5s2.462-5.5 5.5-5.5c1.481 0 2.811.59 3.807 1.541l-1.6 1.541a3.085 3.085 0 0 0-2.207-.907c-1.897 0-3.444 1.565-3.444 3.425s1.547 3.425 3.444 3.425c1.694 0 2.909-1.212 3.156-2.832h-3.156v-2.017h5.272c.056.282.101.564.101.936 0 3.21-2.151 5.488-5.373 5.488z"/>
+                  </svg>
+                  Apple
+                </a>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
